7a040d95627f709bbc20fb9ea06fe75e5a40a576
Jeremy Mullen <jeremymullen@stockx.com>
2018-12-03 13:52:57 -0500

    Complete part 2

--- day02/day02.js
+++ day02/day02.js
@@ -4,6 +4,7 @@ const input = fs.readFileSync('input.txt').toString().split('\n');
 
 let trips = 0;
 let dubs = 0;
+let isAnswered = false;
 
 for (let element of input) {
 
@@ -22,8 +23,25 @@ for (let element of input) {
     if (array.includes(3)) {
         trips++;
     }
+
+    input.forEach((str) => {
+        let difference = 0;
+        let commonChars = '';
+
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] !== element[i]) {
+                difference++;
+            } else {
+                commonChars += str[i];
+            }
+        }
+
+        if (difference === 1 && !isAnswered) {
+            console.log('Part 2: ' + commonChars);
+            isAnswered = true;
+        }
+    });
+
 }
 
-let checkSum = dubs * trips;
-
-console.log('Part 1: ' + checkSum);
+console.log('Part 1: ' + dubs * trips);
