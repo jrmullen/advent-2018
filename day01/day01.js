444ceef578e38c8e58909086a297813cbb8dc8d5
Jeremy Mullen <jeremymullen@stockx.com>
2018-12-02 22:48:41 -0500

    Complete part 2

--- day01/day01.js
+++ day01/day01.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('input.txt').toString().split('\n').map(x => parseInt(x.trim()));;
+const input = fs.readFileSync('input.txt').toString().split('\n').map(x => parseInt(x.trim()));
 
+// Part 1
 let frequency = 0;
 
 for (let element of input) {
@@ -9,3 +10,18 @@ for (let element of input) {
 }
 
 console.log('Part 1: ' + frequency);
+
+// Part 2
+frequency = 0;
+let frequencies = [];
+
+for (let i = 0; i < input.length; i = (i + 1) % input.length) {
+    frequency += input[i];
+
+    if (frequencies.includes(frequency)) {
+        console.log('Part 2: ' + frequency);
+        break;
+    }
+
+    frequencies.push(frequency);
+}
