7d4a13b30844784cc0c042fb403abfe1e5dafeb4
Jeremy Mullen <jeremymullen@stockx.com>
2018-12-03 10:40:42 -0500

    Combine solutions into the same loop

--- day01/day01.js
+++ day01/day01.js
@@ -2,21 +2,18 @@ const fs = require('fs');
 
 const input = fs.readFileSync('input.txt').toString().split('\n').map(x => parseInt(x.trim()));
 
-// Part 1
 let frequency = 0;
-
-for (let element of input) {
-    frequency += element;
-}
-
-console.log('Part 1: ' + frequency);
-
-// Part 2
-frequency = 0;
+let isAnswered = false;
 let frequencies = [];
 
 for (let i = 0; i < input.length; i = (i + 1) % input.length) {
+
     frequency += input[i];
+
+    if (i === input.length - 1 && !isAnswered) {
+        console.log('Part 1: ' + frequency);
+        isAnswered = true;
+    }
 
     if (frequencies.includes(frequency)) {
         console.log('Part 2: ' + frequency);
